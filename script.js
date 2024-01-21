30164957debacb7fc40c3375709206d3f9b23fc7
bdmanik97 <inmanik97@gmail.com>
2024-01-22 00:26:01 +0600

    function final

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// console.log('hellow world');
+
+// for(let i=0; i<=10; i++){
+//     console.log(i);
+// }
+
+// Function structer start here
+// function show (){
+//     console.log('hellow world');
+
+//     for(let i=0; i<=10; i++){
+//         console.log(i);
+//     }
+// }
+// Function structer end here
+
+// Function Intiger start here
+// function sum(){
+//     event.preventDefault();
+
+//     let number1 = document.getElementById('number1').value;
+//     let number2 = document.getElementById('number2').value;
+
+//     let numberInt1 = parseInt(number1);
+//     let numberInt2 = parseInt(number2);
+
+//     let result = numberInt1 + numberInt2;
+//     console.log(result);
+// }
+// Function Intiger end here
+
+
+// Function Float start here
+function sum(){
+    event.preventDefault();
+
+    let number1 = document.getElementById('number1').value;
+    let number2 = document.getElementById('number2').value;
+
+    let numberInt1 = parseFloat(number1);
+    let numberInt2 = parseFloat(number2);
+
+    let result = numberInt1 + numberInt2;
+    console.log(result);
+}
+// Function Float end here
